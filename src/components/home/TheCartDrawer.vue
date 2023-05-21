17d2bbd6b8bfdd7f626fdc3c3ab66a75461c1f50
Trang Nguyen <ntttrang.20it2@vku.udn.vn>
2023-05-22 00:08:54 +0700

    up code

--- src/components/home/TheCartDrawer.vue
+++ src/components/home/TheCartDrawer.vue
@@ -1,5 +1,69 @@
 <template>
-  <div>hihi</div>
+  <div class="items">
+    <div class="item">
+      <div class="left">
+        <img src="http://res.cloudinary.com/huyennhat/image/upload/v1684683502/images/products/gbpcybctohctifa1jbtc.png" alt="">
+      </div>
+      <div class="right">
+        <div class="name">
+          <p>Hành Trình Về Phương Đông (Tái Bản 2021)</p>
+        </div>
+        <div class="subInfo">
+          <p>Số lượng: 10</p>
+          <p>Giá: 50.000đ</p>
+        </div>
+      </div>
+      <img class="icon-delete" src="../../assets/images/delete.png" alt="" />
+    </div>
+    <div class="item">
+      <div class="left">
+        <img src="http://res.cloudinary.com/huyennhat/image/upload/v1684683502/images/products/gbpcybctohctifa1jbtc.png" alt="">
+      </div>
+      <div class="right">
+        <div class="name">
+          <p>Điềm Tĩnh Và Nóng Giận</p>
+        </div>
+        <div class="subInfo">
+          <p>Số lượng: 10</p>
+          <p>Giá: 50.000đ</p>
+        </div>
+      </div>
+      <img class="icon-delete" src="../../assets/images/delete.png" alt="" />
+    </div>
+    <div class="item">
+      <div class="left">
+        <img src="http://res.cloudinary.com/huyennhat/image/upload/v1684683502/images/products/gbpcybctohctifa1jbtc.png" alt="">
+      </div>
+      <div class="right">
+        <div class="name">
+          <p>Hoa Vẫn Nở Mỗi Ngày</p>
+        </div>
+        <div class="subInfo">
+          <p>Số lượng: 10</p>
+          <p>Giá: 50.000đ</p>
+        </div>
+      </div>
+      <img class="icon-delete" src="../../assets/images/delete.png" alt="" />
+    </div>
+    <div class="item">
+      <div class="left">
+        <img src="http://res.cloudinary.com/huyennhat/image/upload/v1684683502/images/products/gbpcybctohctifa1jbtc.png" alt="">
+      </div>
+      <div class="right">
+        <div class="name">
+          <p>Hoa Vẫn Nở Mỗi Ngày</p>
+        </div>
+        <div class="subInfo">
+          <p>Số lượng: 10</p>
+          <p>Giá: 50.000đ</p>
+        </div>
+      </div>
+      <img class="icon-delete" src="../../assets/images/delete.png" alt="" />
+    </div>
+    <div class="button">
+      <button>Tiến hành đặt mua</button>
+    </div>
+  </div>
 </template>
 <script>
 import { defineComponent } from "vue";
@@ -8,3 +72,56 @@ export default defineComponent({
 
 });
 </script>
+<style>
+  .items .item{
+    display: flex;
+    margin-bottom: 12px;
+  }
+  .items .item .icon-delete{
+    height: 25px;
+    display: block;
+    margin: auto 0;
+    cursor: pointer;
+  }
+
+  .items .item .left {
+    width: 30%;
+  }
+  .items .item .left img{
+    max-width: 100%;
+    max-height: 100%;
+  }
+  .items .item .right {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .items .item .right .name p{
+    margin-bottom: 0px;
+    font-size: 16px;
+    font-weight: 600;
+    color: black;
+  }
+  .items .item .right .subInfo p{
+    margin-bottom: -2px;
+    font-size: 14px;
+    font-weight: 400;
+    color: #555;
+  }
+
+  .items .button {
+    display: flex;
+    justify-content: center;
+    margin-top: 40px;
+  }
+  .items .button button{
+    background: green;
+    border: none;
+    padding: 10px 20px;
+    color: white;
+    cursor: pointer;
+    font-size: 15px;
+    font-weight: 600;
+  }
+</style>
